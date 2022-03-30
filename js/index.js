2968417db4a41acda6a229c0df947802077e1669
Jiek Ruan <ruanjiek2001@gmail.com>
2022-03-30 16:16:48 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/*Faire apparaitre le menu caché*/
+// const navMenu = document.getElementById('nav-menu'),
+//     navToggle = document.getElementById('nav-toggle'),
+//     navClose = document.getElementById('nav-close')
+// if(navToggle){
+//     navToggle.addEventListener('click', () => {
+//         navMenu.classList.add('show-menu')
+//     })
+// }
